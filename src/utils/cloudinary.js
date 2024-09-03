0d18303987fc4aeb5188060c447ff9552b2f408e
abhishekTiwari7275 <abhishek.tiwari1@mail.vinove.com>
2024-09-03 10:36:10 +0530

    Chnages Done Cart Api Inseterd

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -8,25 +8,50 @@ import fs from 'fs'
     api_secret:'VjP7m_3ZAmKcic9NIWjkRXtKDrQ' 
   });
   
+  const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        //upload the file on cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        // file has been uploaded successfull
+        //console.log("file is uploaded on cloudinary ", response.url);
+        fs.unlinkSync(localFilePath)
+        return response;
 
-const uploadOnCloudinary = async (localFilePath) => {
-    try{
-        if(!localFilePath) return null
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed
+        return null;
+    }
+}
 
-        //upload this file on cloudinary 
+const deleteFromCloudinary = async(file_public_id) => {
+    try {
 
-       const response =  await cloudinary.uploader.upload(localFilePath, { resource_type:"auto"})
+        if (!file_public_id) {
+            console.log("File not found");
+            return null;
+        }
 
-        fs.unlinkSync(localFilePath) // remove the locally saved temparary file upload operation got failed
+        const response = await cloudinary.uploader.destroy(file_public_id, {
+            resource_type: "auto"
+        });
+
+        if (response.result === "ok") {
+            console.log(`Successfully deleted file with public ID: ${file_public_id}`);
+        } else {
+            console.log(`Failed to delete file: ${response.result}`);
+        }
+
+        return response;
         
-        return response
-
-    }catch(error){
-        console.log(error)
-        fs.unlinkSync(localFilePath) // remove the locally saved temparary file upload operation got failed
-        return null
+    } catch (error) {
+        console.log("error while deleting the file");
+        throw error;
     }
 }
 
 
-export {uploadOnCloudinary}
+
+  export {uploadOnCloudinary, deleteFromCloudinary}
